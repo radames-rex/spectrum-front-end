860c9716b9c2e721817c3ac6e69003d2410497f8
radames-rex <radames.rex@gmail.com>
2017-02-03 14:29:12 -0300

    ficou muito alegre

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 "strict mode";
 
 (function() {
-  var elem;
+  var elem,
+    k = [];
   for (var i = 0; i < 18; i++) {
     elem = document.getElementsByClassName('spectrum__item');
-    elem[i].style.backgroundColor = "rgb("+i*2+","+i*8+","+i*8+")";
+    if(i < 3){
+      k = [8,2,8];
+      elem[i].style.backgroundColor = "rgb("+(((i%3)+(i+3))*k[0])+","+(((i%3)+(i+3))*k[1])+","+(((i%3)+(i+3))*k[2])+")";
+    }
+    if(i < 6 && i >= 3){
+      k = [2,8,8];
+      elem[i].style.backgroundColor = "rgb("+(((i%3)+(i+3))*k[0])+","+(((i%3)+(i+3))*k[1])+","+(((i%3)+(i+3))*k[2])+")";
+    }
+    if(i < 9 && i >= 6){
+      k = [2,2,8];
+      elem[i].style.backgroundColor = "rgb("+(((i%3)+(i+3))*k[0])+","+(((i%3)+(i+3))*k[1])+","+(((i%3)+(i+3))*k[2])+")";
+    }
+    if(i < 12 && i >= 9){
+      k = [2,8,2];
+      elem[i].style.backgroundColor = "rgb("+(((i%3)+(i+3))*k[0])+","+(((i%3)+(i+3))*k[1])+","+(((i%3)+(i+3))*k[2])+")";
+    }
+    if(i < 15 && i >= 12){
+      k = [8,8,2];
+      elem[i].style.backgroundColor = "rgb("+(((i%3)+(i+3))*k[0])+","+(((i%3)+(i+3))*k[1])+","+(((i%3)+(i+3))*k[2])+")";
+    }
+    if(i < 18 && i >= 15){
+      k = [8,2,2];
+      elem[i].style.backgroundColor = "rgb("+(((i%3)+(i+3))*k[0])+","+(((i%3)+(i+3))*k[1])+","+(((i%3)+(i+3))*k[2])+")";
+    }
   }
 })();
